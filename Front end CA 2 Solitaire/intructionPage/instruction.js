4111296e9c6ef23039e88dae79e2e3cf0394ba53
Sahil Kharatmol <144338853+SahilK1720@users.noreply.github.com>
2023-11-21 20:11:07 +0530

    Add files via upload

---
+++ Front end CA 2 Solitaire/intructionPage/instruction.js
@@ -0,0 +1,7 @@
+document.getElementById("proceed-button").addEventListener("click", function(e) {
+    location.href = "../gamePage/game.html";
+} );
+
+document.getElementById("back").addEventListener("click", function(e) {
+    location.href = "../index.html";
+} );

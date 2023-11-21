c31787dd225300b0682473c30740a702235f67e8
Sahil Kharatmol <144338853+SahilK1720@users.noreply.github.com>
2023-11-21 20:11:42 +0530

    Delete Front end CA 2 Solitaire directory

--- Front end CA 2 Solitaire/intructionPage/instruction.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("proceed-button").addEventListener("click", function(e) {
-    location.href = "../gamePage/game.html";
-} );
-
-document.getElementById("back").addEventListener("click", function(e) {
-    location.href = "../index.html";
-} );
--- Front end CA 2 Solitaire/resultPage/result.js
+++
@@ -1,6 +0,0 @@
-const playAgain = document.getElementById("play-again");
-
-playAgain.addEventListener("click", function(){
-    location.href = "../index.html";
-})
-
